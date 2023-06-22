12f1838379857a34170e37e30f5864ed7d2a1924
Gustavo Henrique <ghenriquesouza11@gmail.com>
2023-06-22 13:11:28 -0300

    cria um botão para apagar uma tarefa

--- frontend/todo-list-frontend/src/components/tasksList.js
+++ frontend/todo-list-frontend/src/components/tasksList.js
@@ -1,19 +1,36 @@
 import React, { useContext, useEffect } from 'react';
+import axios from 'axios';
 import { fetchContext } from '../contexts/fetchContext';
 
 function TasksList() {
   const { getTasks, tasks } = useContext(fetchContext);
+
   useEffect(() => {
     getTasks();
   }, [getTasks, tasks]);
+
+  const deleteTask = (id) => {
+    axios.delete(`http://localhost:3333/tasks/${id}`);
+  };
+
   return (
-    <>
+    <ul>
       {
-        tasks.map((task) => (
-          <h1 key={ task.id }>{task.title}</h1>
-        ))
+        tasks.map((task) => {
+          const { title, id } = task;
+          return (
+            <li key={ id }>
+              <h1>{title}</h1>
+              <button
+                onClick={ () => deleteTask(id) }
+              >
+                Apagar
+              </button>
+            </li>
+          );
+        })
       }
-    </>
+    </ul>
   );
 }
 
